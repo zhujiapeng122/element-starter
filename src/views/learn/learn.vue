a414a9ea25e88a77d63ada20488eaddc3dd2a35c
zhujiapeng122 <820639972@qq.com>
2020-11-19 10:24:53 +0800

    初始化

---
+++ src/views/learn/learn.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-container>
+    <el-aside width="200px">
+      <el-menu>
+        <el-submenu index="1">菜单一</el-submenu>
+          <el-menu-item index="1-1">选项一</el-menu-item>
+        <el-menu-item index="1-2">选项二</el-menu-item>
+      </el-menu>
+    </el-aside>
+
+    <el-container>
+      <el-header>
+
+      </el-header>
+
+      <el-main>
+
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+  export default {
+    created() {
+      this.init();
+    },
+    methods: {
+      init() {
+        this.$axios.post("/menu", {
+          prdNo: "123",
+          amount: 789.00
+        }).then(res =>{
+          console.log(res);
+        })
+      }
+    }
+  }
+</script>
